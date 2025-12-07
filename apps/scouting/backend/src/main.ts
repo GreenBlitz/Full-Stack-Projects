bacc9eb0f61147d55ff38da8e46757c38cea54db
RJ0907 <ranelj2008@gmail.com>
2025-12-07 20:09:58 +0200

    Ran - started writing the test

--- apps/scouting/backend/src/main.ts
+++ apps/scouting/backend/src/main.ts
@@ -1,6 +1,8 @@
 // בס"ד
 import express from "express";
 import { apiRouter } from "./routes";
+import { GameObject } from "../game-object";
+import { CoralEvent, AlgaeEvent, GameEventsCounter } from "./game-events";
 
 const app = express();
 
@@ -12,3 +14,25 @@ app.use("/api/v1", apiRouter);
 app.listen(port, () => {
   console.log(`Production server running at http://localhost:${port}`);
 });
+
+const coralCounter: GameEventsCounter<CoralEvent> = {
+  L1: 0,
+  L2: 0,
+  L3: 0,
+  L4: 0
+}
+
+const algaeCounter: GameEventsCounter<AlgaeEvent> = {
+  Processor: 0,
+  Net: 0
+}
+
+const coral: GameObject<CoralEvent> = {
+  name: "coral",
+  gameEvents: coralCounter
+}
+
+const algae: GameObject<AlgaeEvent> = {
+  name: "algae",
+  gameEvents: algaeCounter
+}
