23df4d79d63a0f85f73a13680d89548297cc89c6
RJ0907 <ranelj2008@gmail.com>
2025-12-07 18:10:01 +0200

    Ran - added coral

---
+++ apps/scouting/backend/src/game-object-coral.ts
@@ -0,0 +1,17 @@
+import { GameObject } from "../game-object";
+import { AlgaeEvent, AllPossibleGameEvents, CoralEvent } from "./game-events";
+
+export function calculatePoints(coral: GameObject<CoralEvent>): number {
+        return coral.gameEvents.L1*L1_SCORING+
+        coral.gameEvents.L2*L2_SCORING+
+        coral.gameEvents.L3*L3_SCORING+
+        coral.gameEvents.L4*L4_SCORING
+    }
+export function calculateRP(): number {
+        return 0
+}
+
+const L1_SCORING: number = 2
+const L2_SCORING: number = 3
+const L3_SCORING: number = 4
+const L4_SCORING: number = 5
