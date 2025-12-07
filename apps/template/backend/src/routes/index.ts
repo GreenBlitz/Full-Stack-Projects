0371b0d4b27c459e66f1ecfb586892c80dc4a8e4
YoniKiriaty <yonikiriaty@gmail.com>
2025-12-07 18:06:12 +0200

    Yoni - template removal

--- apps/template/backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-// בס"ד
-import { Router } from "express";
-import { StatusCodes } from "http-status-codes";
-
-export const apiRouter = Router();
-
-apiRouter.get("/health", (req, res) => {
-  res.status(StatusCodes.OK).send({ message: "Healthy!" });
-});
