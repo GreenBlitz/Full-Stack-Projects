6cee1c697b3714c882ded997a72ffd24c2ac2371
RJ0907 <ranelj2008@gmail.com>
2025-12-07 19:29:48 +0200

    Ran - added a vital interface

---
+++ apps/scouting/backend/src/scoring-data.ts
@@ -0,0 +1,8 @@
+import { GameObject } from "../game-object";
+import { AlgaeEvent, AllPossibleGameEvents, CoralEvent } from "./game-events";
+
+export interface GameObjectScoringData<T extends AllPossibleGameEvents>{
+    gameObject: GameObject<T>
+    pointsScoredWithGameObject: number
+    rpScoredWithGameObject: number
+}
