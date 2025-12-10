11f72fd0c937bc8fd1af3683a4dcde56c81ca950
RJ0907 <ranelj2008@gmail.com>
2025-12-10 17:04:07 +0200

    Ran - hopefully fixed

--- apps/scouting/backend/scoring-calculator.ts
+++ apps/scouting/backend/scoring-calculator.ts
@@ -7,20 +7,20 @@ export interface ScoringCalculator<T extends AllPossibleGameEvents>{
 }
 
 export function addScoring<T extends AllPossibleGameEvents>(scoringCalculator: ScoringCalculator<T>,
-    gameObject: GameObject<T>, calculatePoints: ()=> number,
-        calculateRP: ()=> number){
+    gameObject: GameObject<T>, calculatePoints: (gameObject: GameObject<T>)=> number,
+        calculateRP: (gameObject: GameObject<T>)=> number){
         const tempGameObjectScoringData: GameObjectScoringData<T> = {
             gameObject: gameObject,
-            pointsScoredWithGameObject: calculatePointsOfAGameObject(gameObject,calculatePoints),
+            pointsScoredWithGameObject: calculatePointsOfAGameObject(gameObject, calculatePoints),
             rpScoredWithGameObject: calculateRPOfAGameObject(gameObject, calculateRP)
         }
         scoringCalculator.gameObjectsScoringData.push(tempGameObjectScoringData)
 }
 
-export function calculatePointsOfAGameObject<T extends AllPossibleGameEvents>(gameObject: GameObject<T>, calculatePoints: ()=> number): number{
-        return calculatePoints()
+export function calculatePointsOfAGameObject<T extends AllPossibleGameEvents>(gameObject: GameObject<T>, calculatePoints: (gameObject: GameObject<T>)=> number): number{
+        return calculatePoints(gameObject)
 }
 
-export function calculateRPOfAGameObject<T extends AllPossibleGameEvents>(gameObject: GameObject<T>, calculateRP: ()=> number): number{
-        return calculateRP()
+export function calculateRPOfAGameObject<T extends AllPossibleGameEvents>(gameObject: GameObject<T>, calculateRP: (gameObject: GameObject<T>)=> number): number{
+        return calculateRP(gameObject)
 }
