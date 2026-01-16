c3bc35fabec2f52a021f4acadfb03a3d8a7c65b5
YoniKiriaty <yonikiriaty@gmail.com>
2026-01-16 12:38:36 +0200

    Yoni - removed unnecessary stuff

--- apps/scouting/common/types/ScoreBreakdown2025.ts
+++ apps/scouting/common/types/ScoreBreakdown2025.ts
@@ -70,6 +70,4 @@ export const scoreBreakdown2025 = t.type({
   wallAlgaeCount: t.number,
 });
 
-const x = t.array(scoreBreakdown2025)
-
 export type ScoreBreakdown2025 = t.TypeOf<typeof scoreBreakdown2025>;
