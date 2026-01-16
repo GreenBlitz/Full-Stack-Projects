88b2486b05f8d6c0eaed4a1a7780715a4000d694
YoniKiriaty <yonikiriaty@gmail.com>
2026-01-16 17:09:34 +0200

    Yoni - return accidently deleted turbo

--- apps/scouting/frontend/src/App.tsx
+++ apps/scouting/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { useState, type FC } from "react";
 const counterStartingValue = 0;
 const countIncrement = 1;
 const maxCountingValue = 5;
+
 const App: FC = () => {
   const [count, setCount] = useState<string | number>(counterStartingValue);
 
