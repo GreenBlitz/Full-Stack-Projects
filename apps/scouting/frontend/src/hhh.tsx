53bf7718b6a421f329ab8041a64cf2b966aa06f3
unknown <yuvalgevabrodt1@gmail.com>
2026-01-19 17:06:33 +0200

    yuvalButtons

--- apps/scouting/frontend/src/hhh.tsx
+++ apps/scouting/frontend/src/hhh.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from "react";
+import React, { type FC } from "react";
+import { type Dispatch, type SetStateAction } from "react";
+import { type ScoutingForm } from "./types";
 
 const inputStyle = {
   padding: "8px",
@@ -8,7 +10,7 @@ const inputStyle = {
   borderRadius: "6px",
 };
 
-const Buttons: FC = () => {
+const Buttons: FC <{ setForm: SetStateAction<Dispatch<ScoutingForm>> }> = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "12px", width: "200px" }}>
       <input type="text" placeholder="Name Field" style={inputStyle} />
