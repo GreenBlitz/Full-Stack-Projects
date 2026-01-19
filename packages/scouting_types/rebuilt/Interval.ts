e7affa34420aec4c930d5b3711e396ba4cb1e7d6
YoniKiriaty <yonikiriaty@gmail.com>
2026-01-19 20:38:59 +0200

    Yoni - updated maxInterval

--- packages/scouting_types/rebuilt/Interval.ts
+++ packages/scouting_types/rebuilt/Interval.ts
@@ -4,5 +4,5 @@ import * as t from "io-ts";
 export const intervalCodec = t.type({ start: t.number, end: t.number });
 export const maxInterval: t.TypeOf<typeof intervalCodec> = {
     start: 0,
-    end: 256
+    end: 255
 };
