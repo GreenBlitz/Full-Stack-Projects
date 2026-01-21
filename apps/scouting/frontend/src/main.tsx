18fef2745079cb9fd407c13d2b2f0512d8cd9443
RJ0907 <ranelj2008@gmail.com>
2026-01-21 19:02:43 +0200

    Ran - prepared for testing

--- apps/scouting/frontend/src/main.tsx
+++ apps/scouting/frontend/src/main.tsx
@@ -3,9 +3,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import Stopwatch from './components/stopwatch'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Stopwatch></Stopwatch>
   </StrictMode>,
 )
