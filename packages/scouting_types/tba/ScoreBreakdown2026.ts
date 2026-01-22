8fa033db924cf03625eca60594bdf903e302b336
YoniKiriaty <yonikiriaty@gmail.com>
2026-01-22 17:13:18 +0200

    Yoni - added the score breakdown for 2026

---
+++ packages/scouting_types/tba/ScoreBreakdown2026.ts
@@ -0,0 +1,58 @@
+// בס"ד
+import * as t from "io-ts";
+
+const climbCodec = t.keyof({
+  None: null,
+  Level1: null,
+  Level2: null,
+  Level3: null,
+});
+
+const scoreSegment = (key: string) => ({
+  [`${key}Count`]: t.number,
+  [`${key}Points`]: t.number,
+});
+
+const hubScoreCodec = t.type({
+  ...scoreSegment("auto"),
+  ...scoreSegment("teleop"),
+
+  ...scoreSegment("transition"),
+  ...scoreSegment("shift1"),
+  ...scoreSegment("shift2"),
+  ...scoreSegment("shift3"),
+  ...scoreSegment("shift4"),
+  ...scoreSegment("endgame"),
+});
+
+export const scoreBreakdown2026 = t.type({
+  adjustPoints: t.number,
+
+  autoTowerPoints: t.number,
+  autoTowerRobot1: climbCodec,
+  autoTowerRobot2: climbCodec,
+  autoTowerRobot3: climbCodec,
+
+  endGameTowerPoints: t.number,
+  endGameTowerRobot1: climbCodec,
+  endGameTowerRobot2: climbCodec,
+  endGameTowerRobot3: climbCodec,
+
+  foulCount: t.number,
+  foulPoints: t.number,
+  g206Penalty: t.boolean,
+  majorFoulCount: t.number,
+  minorFoulCount: t.number,
+
+  hubScore: hubScoreCodec,
+
+  rp: t.number,
+  energizedAchieved: t.boolean,
+  superchargedAchieved: t.boolean,
+  traversalAchived: t.boolean,
+
+  totalAutoPoints: t.number,
+  totalTeleopPoints: t.number,
+  totalTowerPoints: t.number,
+  totalPoints: t.number,
+});
