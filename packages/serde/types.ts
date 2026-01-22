ba6d61783ba28a524ca0cc02f7dd60b418f0b0df
YoniKiriaty <yonikiriaty@gmail.com>
2026-01-22 19:14:12 +0200

    Yoni - removed types comments

--- packages/serde/types.ts
+++ packages/serde/types.ts
@@ -2,12 +2,9 @@
 import type { BitArray } from "./BitArray";
 
 export type Serializer<T> = (seriailzedData: BitArray, data: T) => void;
-// return field name, and its appropriate Serializer
 export type RecordSerializer<T> = Record<string, Serializer<T>>;
 
-// returns the deserializedData
 export type Deserializer<T> = (serializedData: BitArray) => T;
-// return field name, and its appropriate Deserializer
 export type RecordDeserializer<T> = Record<string, Deserializer<T>>;
 
 export interface FieldsRecordSerde<T> {
