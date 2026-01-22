f2cec3d405065edce67266dc02c19273b7214fed
RJ0907 <ranelj2008@gmail.com>
2026-01-22 18:57:40 +0200

    Ran - works and ready to be merged"

--- apps/scouting/frontend/src/main.tsx
+++ apps/scouting/frontend/src/main.tsx
@@ -3,9 +3,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Stopwatch from "./components/Stopwatch";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <App />
   </StrictMode>,
 );
