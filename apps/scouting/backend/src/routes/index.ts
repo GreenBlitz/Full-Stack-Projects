b2bce1fac0281c0fbcf6b93a1ee1b460af6e4c57
YoniKiriaty <yonikiriaty@gmail.com>
2026-01-25 19:05:05 +0200

    Yoni - fixed bad task

--- apps/scouting/backend/src/routes/index.ts
+++ apps/scouting/backend/src/routes/index.ts
@@ -4,14 +4,10 @@ import { StatusCodes } from "http-status-codes";
 import { tbaRouter } from "./tba";
 import { gameRouter } from "./game-router";
 
-
-
 export const apiRouter = Router();
-
 
 apiRouter.use("/tba", tbaRouter);
 apiRouter.use("/game", gameRouter);
 apiRouter.get("/health", (req, res) => {
   res.status(StatusCodes.OK).send({ message: "Healthy!" });
 });
-
