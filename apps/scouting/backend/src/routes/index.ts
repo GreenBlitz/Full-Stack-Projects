f063e55d589171f3e01279fd684b1eb41f55df32
YoniKiriaty <yonikiriaty@gmail.com>
2026-01-28 11:32:44 +0200

    Yoni - removed useless route

--- apps/scouting/backend/src/routes/index.ts
+++ apps/scouting/backend/src/routes/index.ts
@@ -2,13 +2,11 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import { tbaRouter } from "./tba";
-import { formsRouter } from "./forms";
 import { gameRouter } from "./game-router";
 
 export const apiRouter = Router();
 
 apiRouter.use("/tba", tbaRouter);
-apiRouter.use("/forms", formsRouter);
 apiRouter.use("/game", gameRouter);
 
 apiRouter.get("/health", (req, res) => {
