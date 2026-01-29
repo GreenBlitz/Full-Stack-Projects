2a770689fc82830941186aa21987cfbcd871987c
unknown <yuvalgevabrodt1@gmail.com>
2026-01-29 19:03:19 +0200

    so i centered the div

--- apps/scouting/frontend/src/PreMatchTab.tsx
+++ apps/scouting/frontend/src/PreMatchTab.tsx
@@ -10,6 +10,7 @@ const inputStyle: React.CSSProperties = {
   fontSize: "20px",
   width: "300px",
   margin: "10px 0",
+
 };
 const MatchNumber = 127;
 const TeamNumber = 16383;
@@ -24,102 +25,101 @@ const Buttons: FC<{
         flexDirection: "column",
         gap: "12px",
         width: "100%",
+        alignItems: "center",
+        margin: "auto", 
+        padding: "20px 0"
+
       }}
     >
-      <div className="flex flex-row items-center w-108">
-      <div className="Design text-green-500">
-        <div className="outputName"> Scouter Name:</div>
-      </div>
-      <div className="ml-auto">
-        <input
-          type="text"
-          placeholder=""
-          style={inputStyle}
-          value={form.scouterName}
-          onChange={(e) => {
-            setForm((prev) => ({ ...prev, scouterName: e.target.value }));
-          }}
-        />
-      </div>
+
+<div className="flex flex-row items-center justify-between w-[460px]">
+        <div className="Design text-green-500">
+          <div className="outputName"> Scouter Name:</div>
+        </div>
+        <div className="ml-auto">
+          <input
+            type="text"
+            placeholder=""
+            style={inputStyle}
+            value={form.scouterName}
+            onChange={(e) => {
+              setForm((prev) => ({ ...prev, scouterName: e.target.value }));
+            }}
+          />
+        </div>
       </div>
 
-      <div className="flex flex-row items-center w-108">
-      <div className="Design  text-green-500">
-        <div className="outputMatch"> Match Number:</div>
-      </div>
-      <div className="ml-auto">
-      <input
-        type="number"
-        placeholder=""
-        style={inputStyle}
-        value={form.matchNumber}
-        min="0"
-        defaultValue=" "
-        max={MatchNumber}
-        onChange={(e) => {
-          setForm((prev) => ({
-            ...prev,
-            matchNumber: parseInt(e.target.value),
-          }));
-        }}
-      />
-      </div>
-      </div>
-
-
-      <div className="flex flex-row items-center w-108">
-      <div className="Design  text-green-500">
-        <div className="outputTeam"> Team Number:</div>
-      </div>
-      <div className="ml-auto">
-
-      <input
-        type="number"
-        placeholder=""
-        style={inputStyle}
-        min=""
-        max={TeamNumber}
-        value={form.teamNumber}
-        onChange={(e) => {
-          setForm((prev) => ({
-            ...prev,
-            teamNumber: parseInt(e.target.value),
-          }));
-        }}
-      />
-      </div>
+<div className="flex flex-row items-center justify-between w-[460px] ">
+        <div className="Design  text-green-500">
+          <div className="outputMatch"> Match Number:</div>
+        </div>
+        <div className="ml-auto">
+          <input
+            type="number"
+            placeholder=""
+            style={inputStyle}
+            value={form.matchNumber}
+            min="0"
+            defaultValue=" "
+            max={MatchNumber}
+            onChange={(e) => {
+              setForm((prev) => ({
+                ...prev,
+                matchNumber: parseInt(e.target.value),
+              }));
+            }}
+          />
+        </div>
       </div>
 
-
-      <div className="flex flex-row items-center w-108">
-      <div className="Design  text-green-500">
-        <div className="outputTeam"> Match Type:</div>
+<div className="flex flex-row items-center justify-between w-[460px] ">
+        <div className="Design  text-green-500">
+          <div className="outputTeam"> Team Number:</div>
+        </div>
+        <div className="ml-auto">
+          <input
+            type="number"
+            placeholder=""
+            style={inputStyle}
+            min=""
+            max={TeamNumber}
+            value={form.teamNumber}
+            onChange={(e) => {
+              setForm((prev) => ({
+                ...prev,
+                teamNumber: parseInt(e.target.value),
+              }));
+            }}
+          />
+        </div>
       </div>
-      <div className="ml-auto">
-      <select
-        style={inputStyle}
-        className=""
-        value={form.matchType}
-        onChange={(e) =>
-          setForm((prev) => ({
-            ...prev,
-            matchType: e.target.value as
-              | "practice"
-              | "qualification"
-              | "playoff",
-          }))
-        }
-      
 
-      >
-        <option value="practice">Practice</option>
-        <option value="qualification">Qualification</option>
-        <option value="playoff">Playoff</option>
-      </select>
+<div className="flex flex-row items-center justify-between w-[460px]">
+        <div className="Design  text-green-500">
+          <div className="outputTeam"> Match Type:</div>
+        </div>
+        <div className="ml-auto">
+          <select
+            style={inputStyle}
+            className=""
+            value={form.matchType}
+            onChange={(e) =>
+              setForm((prev) => ({
+                ...prev,
+                matchType: e.target.value as
+                  | "practice"
+                  | "qualification"
+                  | "playoff",
+              }))
+            }
+          >
+            <option value="practice">Practice</option>
+            <option value="qualification">Qualification</option>
+            <option value="playoff">Playoff</option>
+          </select>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-
   );
 };
 export { Buttons };
