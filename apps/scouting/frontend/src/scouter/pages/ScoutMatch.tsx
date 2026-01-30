63a33442c0797eac8efd10fe1eb6a271213740dd
yoavs8 <shalev.yoav@gmail.com>
2026-01-30 13:47:09 +0200

    yoni loves boys

--- apps/scouting/frontend/src/scouter/pages/ScoutMatch.tsx
+++ apps/scouting/frontend/src/scouter/pages/ScoutMatch.tsx
@@ -15,7 +15,9 @@ import {
 } from "@repo/scouting_types";
 import { ShiftTab } from "./tabs/ShiftTab";
 import { useLocalStorage } from "@repo/local_storage_hook";
-import { PostMatchTab } from "./tabs/PostMatchTab";
+import { PostMatchTab } from "./tabs/PostMatchTab"
+import { AutoTab } from "./tabs/AutoTab";
+import { ClimbTab } from "./tabs/ClimbTab";
 export interface TabProps {
   setForm: Dispatch<SetStateAction<ScoutingForm>>;
   currentForm: ScoutingForm;
