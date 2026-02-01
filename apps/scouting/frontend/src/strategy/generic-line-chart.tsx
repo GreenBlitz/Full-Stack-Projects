102a8d63d4f6d08e9ddaf845a2c09391810a5d26
RJ0907 <ranelj2008@gmail.com>
2026-02-01 20:22:43 +0200

    Ran - downloaded Chart JS

---
+++ apps/scouting/frontend/src/strategy/generic-line-chart.tsx
@@ -0,0 +1,29 @@
+//בס"ד
+import { Line } from 'react-chartjs-2';
+
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+} from 'chart.js';
+
+ChartJS.register(
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
+export const LineGraph = () =>{
+    return <Line></Line>
+}
