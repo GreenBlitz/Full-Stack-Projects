ab5f16de4dbf47fecf1745df120da9b39f83bf25
RJ0907 <ranelj2008@gmail.com>
2026-02-03 19:55:36 +0200

    Ran - linting shit

--- apps/scouting/frontend/src/main.tsx
+++ apps/scouting/frontend/src/main.tsx
@@ -3,14 +3,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Line } from "react-chartjs-2";
-import { LineGraph, PieGraph } from "./strategy/generic-line-chart";
-import {
-  lineChartProps,
-  scoringQual1,
-} from "./strategy/datasets.ts/test-dataset";
+import { PieGraph } from "./strategy/generic-line-chart";
+import { scoringQual1 } from "./strategy/datasets.ts/test-dataset";
 
 registerSW({ immediate: true });
 
