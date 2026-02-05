70305bb89acec9a30d71310296cc55d623c9604a
YoniKiriaty <yonikiriaty@gmail.com>
2026-02-05 16:41:42 +0200

    Yoni - made casting better

--- packages/type-utils/index.ts
+++ packages/type-utils/index.ts
@@ -1,6 +1,6 @@
 // בס"ד
-import { flow } from "fp-ts/lib/function";
-import { type Either, flatMap, fold } from "fp-ts/lib/Either";
+import { constFalse, constTrue, flow, identity } from "fp-ts/lib/function";
+import { type Either, fold, orElse } from "fp-ts/lib/Either";
 import type { Refinement } from "fp-ts/lib/Refinement";
 import { left, right } from "fp-ts/lib/Either";
 import { mapObject } from "@repo/array-functions";
@@ -11,35 +11,22 @@ export const isObject = (value: unknown): value is object =>
 const isStringedNumber = (value: unknown): value is `${number}` =>
   typeof value === "string" && !isNaN(Number(value)) && value !== "";
 
-export const createTypeGuard = <A, B extends A, C>(
-  refinement: Refinement<A, B>,
-  onTrue: (item: B) => C,
-): (<E1>(ma: Either<E1, A>) => Either<C | E1, A>) =>
-  flow(
-    flatMap((item: A) => (refinement(item) ? left(onTrue(item)) : right(item))),
-  );
+export const createTypeGuard = <E, A extends E, B>(
+  refinement: Refinement<E, A>,
+  onTrue: (item: A) => B,
+): (<C>(ma: Either<E, B | C>) => Either<E, B | C>) =>
+  orElse((item: E) => (refinement(item) ? right(onTrue(item)) : left(item)));
 
 type CastedItem = object | number | string | boolean | CastedItem[];
 export const castItem: (item: unknown) => CastedItem = flow(
-  right,
+  left,
+  
   createTypeGuard(isObject, (obj) => mapObject(obj, castItem)),
   createTypeGuard(Array.isArray, (item) => item.map(castItem)),
   createTypeGuard(isStringedNumber, Number),
-  createTypeGuard(
-    (item) => item === "true",
-    () => true,
-  ),
-  createTypeGuard(
-    (item) => item === "false",
-    () => false,
-  ),
-  createTypeGuard(
-    (item) => typeof item === "string",
-    (item) => item,
-  ),
+  createTypeGuard((item) => item === "true", constTrue),
+  createTypeGuard((item) => item === "false", constFalse),
+  createTypeGuard((item) => typeof item === "string", identity),
 
-  fold(
-    (item) => item,
-    () => "",
-  ),
+  fold(() => "", identity),
 );
