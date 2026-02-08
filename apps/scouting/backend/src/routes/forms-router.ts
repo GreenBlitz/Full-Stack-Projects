fdff18740fc8030cdf4d58378e947ccb2ab36193
YoniKiriaty <yonikiriaty@gmail.com>
2026-02-08 18:14:41 +0200

    Yoni - simplified

--- apps/scouting/backend/src/routes/forms-router.ts
+++ apps/scouting/backend/src/routes/forms-router.ts
@@ -1,10 +1,9 @@
 // בס"ד
 
-import { type Request, Router } from "express";
+import { Router } from "express";
 import { flow, pipe } from "fp-ts/lib/function";
 import { getDb } from "../middleware/db";
 import {
-  filterOrElse,
   flatMap,
   fold,
   fromEither,
@@ -15,9 +14,7 @@ import { StatusCodes } from "http-status-codes";
 import { createBodyVerificationPipe } from "../middleware/verification";
 import { right } from "fp-ts/lib/Either";
 import { mongofyQuery } from "../middleware/query";
-import type { Type } from "io-ts";
 import * as t from "io-ts";
-import { isEmpty } from "@repo/array-functions";
 
 export const formsRouter = Router();
 
@@ -39,40 +36,24 @@ formsRouter.get("/", async (req, res) => {
   )();
 });
 
-const getCollectionAndBody = <U>(req: Request, codec: Type<U, unknown>) =>
-  pipe(
+const combinedCodec = t.union([scoutingFormCodec, t.array(scoutingFormCodec)]);
+
+formsRouter.post("/", async (req, res) => {
+  await pipe(
     getCollection(),
     flatMap((collection) =>
       pipe(
         right(req),
-        createBodyVerificationPipe(codec),
+        createBodyVerificationPipe(combinedCodec),
         fromEither,
         map((body) => ({ collection, body })),
       ),
     ),
-  );
-
-formsRouter.post("/single", async (req, res) => {
-  await pipe(
-    getCollectionAndBody(req, scoutingFormCodec),
-    map(({ collection, body }) => collection.insertOne(body)),
-    fold(
-      (error) => () =>
-        Promise.resolve(res.status(error.status).send(error.reason)),
-      (result) => async () =>
-        res.status(StatusCodes.OK).json({ result: await result }),
+    map(({ collection, body }) =>
+      Array.isArray(body)
+        ? collection.insertMany(body)
+        : collection.insertOne(body),
     ),
-  )();
-});
-
-formsRouter.post("/multiple", async (req, res) => {
-  await pipe(
-    getCollectionAndBody(req, t.array(scoutingFormCodec)),
-    filterOrElse(
-      ({ body }) => isEmpty(body),
-      () => ({ status: StatusCodes.BAD_REQUEST, reason: `Empty Form Array` }),
-    ),
-    map(({ collection, body }) => collection.insertMany(body)),
     fold(
       (error) => () =>
         Promise.resolve(res.status(error.status).send(error.reason)),
