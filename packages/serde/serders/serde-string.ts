fd0dd88db3cecf43380a4f5d04f6bb42dfcea3b9
Yoni Kiriaty <YoniKiriaty@gmail.com>
2026-02-11 15:59:37 +0200

    Let it 🔥❄️ (#34)
    * Yoni - let it happen and go
    
    * Yoni - removed unimportant thing

--- packages/serde/serders/serde-string.ts
+++ packages/serde/serders/serde-string.ts
@@ -10,25 +10,25 @@ export const serdeString = (): Serde<string> => ({
 
     serdeUnsignedInt(stringLengthBitCount).serializer(
       serializedData,
-      encodedString.length
+      encodedString.length,
     );
     serializedData.insertUInt8Array(
       encodedString,
-      encodedString.length * BIT_ARRAY_LENGTH
+      encodedString.length * BIT_ARRAY_LENGTH,
     );
   },
   deserializer(serializedData: BitArray): string {
     const encodedStringLength =
       serdeUnsignedInt(stringLengthBitCount).deserializer(serializedData);
     const encodedString = serializedData.consumeBits(
-      encodedStringLength * BIT_ARRAY_LENGTH
+      encodedStringLength * BIT_ARRAY_LENGTH,
     );
     return new TextDecoder().decode(encodedString);
   },
 });
 
 export const serdeEnumedString = <Options extends string>(
-  possibleValues: Options[]
+  possibleValues: Options[],
 ): Serde<Options> => {
   function bitsNeeded(possibleValuesCount: number): number {
     return Math.ceil(Math.log2(possibleValuesCount));
@@ -37,14 +37,57 @@ export const serdeEnumedString = <Options extends string>(
     serializer(serializedData: BitArray, data: Options) {
       const neededBits = bitsNeeded(possibleValues.length);
 
-      for (let i = 0; i < possibleValues.length; i++) {
+      for (
+        let /*it go ❄️: The snow glows white on the mountain tonight
+Not a footprint to be seen
+A kingdom of isolation
+And it looks like I'm the queen
+The wind is howling like this swirling storm inside
+Couldn't keep it in, heaven knows I tried
+Don't let them in, don't let them see
+Be the good girl you always have to be
+Conceal, don't feel, don't let them know
+Well, now they know
+Let it go, let it go
+Can't hold it back anymore
+Let it go, let it go
+Turn away and slam the door
+I don't care what they're going to say
+Let the storm rage on
+The cold never bothered me anyway
+It's funny how some distance makes everything seem small
+And the fears that once controlled me can't get to me at all
+It's time to see what I can do
+To test the limits and break through
+No right, no wrong, no rules for me
+I'm free
+Let it go, let it go
+I am one with the wind and sky
+Let it go, let it go
+You'll never see me cry
+Here I stand and here I stay
+Let the storm rage on
+My power flurries through the air into the ground
+My soul is spiraling in frozen fractals all around
+And one thought crystallizes like an icy blast
+I'm never going back, the past is in the past
+Let it go, let it go
+And I'll rise like the break of dawn
+Let it go, let it go
+That perfect girl is gone
+Here I stand in the light of day
+Let the storm rage on
+The cold never bothered me anyway */ i = 0;
+        i < possibleValues.length;
+        i++
+      ) {
         if (data == possibleValues[i]) {
           serdeUnsignedInt(neededBits).serializer(serializedData, i);
           return;
         }
       }
       throw new Error(
-        `value ${data} is not included in possible values: ${possibleValues.toString()}`
+        `value ${data} is not included in possible values: ${possibleValues.toString()}`,
       );
     },
     deserializer(serializedData: BitArray): Options {
@@ -55,7 +98,7 @@ export const serdeEnumedString = <Options extends string>(
         return possibleValues[valueIdentifier];
       }
       throw new Error(
-        `valueIdentifier ${valueIdentifier} does not exist in possible values: ${possibleValues.toString()}`
+        `valueIdentifier ${valueIdentifier} does not exist in possible values: ${possibleValues.toString()}`,
       );
     },
   };
