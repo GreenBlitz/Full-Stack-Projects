c8264de5f5ff95b4d423a382e00f8b69917cdd82
RJ0907 <ranelj2008@gmail.com>
2026-02-15 17:40:11 +0200

    Ran - changed the order of components

--- apps/scouting/frontend/src/PreMatchTab.tsx
+++ apps/scouting/frontend/src/PreMatchTab.tsx
@@ -13,12 +13,12 @@ const PreMatchTab: FC<TabProps> = ({ currentForm: form, setForm }) => {
             type="text"
             className="inputStyle w-[340px] h-full"
             value={form.scouterName}
-            onChange={(event) =>
-              {setForm((prev) => ({
+            onChange={(event) => {
+              setForm((prev) => ({
                 ...prev,
                 scouterName: event.target.value,
-              }))}
-            }
+              }));
+            }}
           />
         </div>
       </div>
@@ -31,16 +31,41 @@ const PreMatchTab: FC<TabProps> = ({ currentForm: form, setForm }) => {
             min={0}
             max={MATCH_NUMBER_MAX}
             value={form.match.number}
-            onChange={(event) =>
-              {setForm((prev) => ({
+            onChange={(event) => {
+              setForm((prev) => ({
                 ...prev,
                 match: {
                   ...prev.match,
                   number: Number(event.target.value),
                 },
-              }))}
-            }
+              }));
+            }}
           />
+        </div>
+      </div>
+      <div className="w-[480px] border-2 border-green-500 rounded-lg p-5 flex flex-col gap-3 py-0 h-15">
+        <div className="flex w-[460px] justify-between items-center text-green-500 pr-[4px] h-70">
+          <div>Match Type:</div>
+          <select
+            className="inputStyle w-[363px] h-full"
+            value={form.match.type}
+            onChange={(event) => {
+              setForm((prev) => ({
+                ...prev,
+                match: {
+                  ...prev.match,
+                  type: event.target.value as
+                    | "practice"
+                    | "qualification"
+                    | "playoff",
+                },
+              }));
+            }}
+          >
+            <option value="practice">Practice</option>
+            <option value="qualification">Qualification</option>
+            <option value="playoff">Playoff</option>
+          </select>
         </div>
       </div>
       <div className="w-[480px] border-2 border-green-500 rounded-lg p-5 flex flex-col gap-3 py-0 h-15">
@@ -52,38 +77,13 @@ const PreMatchTab: FC<TabProps> = ({ currentForm: form, setForm }) => {
             min={0}
             max={TEAM_NUMBER_MAX}
             value={form.teamNumber}
-            onChange={(event) =>
-              {setForm((prev) => ({
+            onChange={(event) => {
+              setForm((prev) => ({
                 ...prev,
                 teamNumber: Number(event.target.value),
-              }))
+              }));
             }}
           />
-        </div>
-      </div>
-      <div className="w-[480px] border-2 border-green-500 rounded-lg p-5 flex flex-col gap-3 py-0 h-15">
-        <div className="flex w-[460px] justify-between items-center text-green-500 pr-[4px] h-70">
-          <div>Match Type:</div>
-          <select
-            className="inputStyle w-[363px] h-full"
-            value={form.match.type}
-            onChange={(event) =>
-              {setForm((prev) => ({
-                ...prev,
-                match: {
-                  ...prev.match,
-                  type: event.target.value as
-                    | "practice"
-                    | "qualification"
-                    | "playoff",
-                },
-              }))
-            }}
-          >
-            <option value="practice">Practice</option>
-            <option value="qualification">Qualification</option>
-            <option value="playoff">Playoff</option>
-          </select>
         </div>
       </div>
     </div>
