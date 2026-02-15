315a5196973c0112b335cd9b5259184c31ee08ee
RJ0907 <ranelj2008@gmail.com>
2026-02-15 18:29:37 +0200

    Ran - done writing except for fetching from TBA

--- apps/scouting/frontend/src/PreMatchTab.tsx
+++ apps/scouting/frontend/src/PreMatchTab.tsx
@@ -1,8 +1,47 @@
 //בס"ד
 import React, { type FC, type Dispatch, type SetStateAction } from "react";
 import type { TabProps } from "./scouter/pages/ScoutMatch";
+import type { Alliance } from "@repo/scouting_types";
 const MATCH_NUMBER_MAX = 127;
 const TEAM_NUMBER_MAX = 16383;
+
+type initialLocation = "close" | "middle" | "far";
+
+interface MatchQualWithTeamNumberProps {
+  qual: number;
+  alliance: Alliance;
+  initialLocation: initialLocation;
+}
+
+const matchQualWithTeamNumber = (
+  matchQualWithTeamNumberProps: MatchQualWithTeamNumberProps,
+) => {
+  const DEFAULT_TEAM_NUMBER = 4590;
+  const CALIBERATION_CONSTANT = 1;
+  const allMatches = await fetchAllAwaitingMatches();
+  if (
+    !allMatches?.[matchQualWithTeamNumberProps.qual - CALIBERATION_CONSTANT]
+  ) {
+    console.error("Match data is unavailable");
+    return DEFAULT_TEAM_NUMBER;
+  }
+  const match =
+    allMatches[matchQualWithTeamNumberProps.qual - CALIBERATION_CONSTANT];
+
+  const alliance: Alliance = matchQualWithTeamNumberProps.alliance;
+
+  switch (matchQualWithTeamNumberProps.initialLocation) {
+    case "close":
+      return alliance === "blue" ? match.blueAlliance[0] : match.redAlliance[0];
+    case "middle":
+      return alliance === "blue" ? match.blueAlliance[1] : match.redAlliance[1];
+    case "far":
+      return alliance === "blue" ? match.blueAlliance[2] : match.redAlliance[2];
+    default:
+      return DEFAULT_TEAM_NUMBER;
+  }
+};
+
 const PreMatchTab: FC<TabProps> = ({ currentForm: form, setForm }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full gap-3  mx-auto">
