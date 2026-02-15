cb9902a96a385a8c03ab46b28c46e65a93e2911c
karnishein <214869392+karnishein@users.noreply.github.com>
2026-02-15 20:48:21 +0200

    karni- added comps

--- packages/scouting_types/rebuilt/scouting_form/Serde.ts
+++ packages/scouting_types/rebuilt/scouting_form/Serde.ts
@@ -13,6 +13,7 @@ import type { Interval } from "./Interval";
 import type { Point, ShootEvent } from "./ShootEvent";
 import type { AutoClimb, defaultAuto, defaultTele } from "./Segments";
 import type { TeleClimb } from "./Shift";
+import { competitionKeys } from "./GameData";
 
 const MATCH_NUMBER_BIT_COUNT = 7;
 const TEAM_NUMBER_BIT_COUNT = 14;
@@ -90,6 +91,7 @@ export const serdeAuto = createRecordSerde<typeof defaultAuto>({
 
 const serdeFields = {
   scouterName: serdeString(),
+  competition: serdeEnumedString(competitionKeys),
   match: createRecordSerde<Match>({
     number: serdeUnsignedInt(MATCH_NUMBER_BIT_COUNT),
     type: serdeEnumedString(["playoff", "qualification", "practice"]),
