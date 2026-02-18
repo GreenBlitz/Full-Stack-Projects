4c1384e1d9da365a5ac7c757fccb9ffc1fd42fa9
Yoni Kiriaty <YoniKiriaty@gmail.com>
2026-02-18 21:13:34 +0200

    Basic Forecast  (#63)
    * Yoni - first version
    
    * Yoni - removed linting stuff
    
    * Yoni - got basic sections good
    
    * Yoni - removed test
    
    * Yoni - split to files

--- apps/scouting/frontend/src/App.tsx
+++ apps/scouting/frontend/src/App.tsx
@@ -4,16 +4,18 @@ import { ScoutMatch } from "./scouter/pages/ScoutMatch";
 import { Route, Routes } from "react-router-dom";
 import { ScoutedMatches } from "./scouter/pages/ScoutedMatches";
 import { TeamTab } from "./strategy/components/TeamTab";
+import { MatchForecast } from "./strategy/components/forecast/MatchForecast";
 import { GeneralDataTable } from "./strategy/GeneralDataTable";
 import { CompareTwo } from "./strategy/CompareTwo";
 
 const App: FC = () => {
   return (
     <Routes>
+      <Route path="*" element={<ScoutedMatches />} />
       <Route path="/scout" element={<ScoutMatch />} />
       <Route path="/team" element={<TeamTab />} />
       <Route path="/general" element={<GeneralDataTable filters={{}} />} />
-      <Route path="*" element={<ScoutedMatches />} />
+      <Route path="/forecast" element={<MatchForecast />} />
       <Route path="/compare" element={<CompareTwo />} />
     </Routes>
   );
