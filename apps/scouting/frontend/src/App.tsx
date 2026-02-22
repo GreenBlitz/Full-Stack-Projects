2c094888ecaa856a98a388be2efbc7400d4c40b0
emmr253 <159184622+emmr253@users.noreply.github.com>
2026-02-22 19:51:10 +0200

    new branch for bps (#71)
    * new branch for bps
    
    * commit before tailwind
    
    * fixed first cr
    
    * okay now i fixed first cr

--- apps/scouting/frontend/src/App.tsx
+++ apps/scouting/frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { TeamTab } from "./strategy/components/TeamTab";
 import { MatchForecast } from "./strategy/components/forecast/MatchForecast";
 import { GeneralDataTable } from "./strategy/GeneralDataTable";
 import { CompareTwo } from "./strategy/CompareTwo";
+import BpsBase from "./scouter/components/bps-components/BpsBase";
 import { Leaderboard } from "./scouter/pages/Leaderboard";
 import { CURRENT_COMPETITION } from "@repo/scouting_types";
 
@@ -19,6 +20,7 @@ const App: FC = () => {
       <Route path="/general" element={<GeneralDataTable filters={{}} />} />
       <Route path="/forecast" element={<MatchForecast />} />
       <Route path="/compare" element={<CompareTwo />} />
+      <Route path="/bps" element={<BpsBase />} />
       <Route
         path="/leaderboard"
         element={<Leaderboard competition={CURRENT_COMPETITION} />}
