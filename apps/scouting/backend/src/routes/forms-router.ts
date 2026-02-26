b5241cefc08450c2d5c2e8e6b311034592a514c9
Yoni Kiriaty <YoniKiriaty@gmail.com>
2026-02-26 22:23:55 +0200

    No Duplicate Forms (#78)
    * Yoni - simplified
    
    * Yoni - fix
    
    * Yoni - safety check
    
    * Yoni - added specification to teamNumber and competition number

--- apps/scouting/backend/src/routes/forms-router.ts
+++ apps/scouting/backend/src/routes/forms-router.ts
@@ -3,13 +3,25 @@
 import { Router } from "express";
 import { flow, pipe } from "fp-ts/lib/function";
 import { getDb } from "../middleware/db";
-import { flatMap, fold, fromEither, map, tryCatch } from "fp-ts/lib/TaskEither";
+import {
+  bind,
+  bindTo,
+  filterOrElse,
+  flatMap,
+  fold,
+  fromEither,
+  map,
+  right,
+  tap,
+  tryCatch,
+} from "fp-ts/lib/TaskEither";
 import { scoutingFormCodec, type ScoutingForm } from "@repo/scouting_types";
 import { StatusCodes } from "http-status-codes";
 import { createBodyVerificationPipe } from "../middleware/verification";
-import { right } from "fp-ts/lib/Either";
+import { right as rightEither } from "fp-ts/lib/Either";
 import { mongofyQuery } from "../middleware/query";
 import * as t from "io-ts";
+import { isEmpty } from "@repo/array-functions";
 
 export const formsRouter = Router();
 
@@ -35,20 +47,35 @@ const combinedCodec = t.union([scoutingFormCodec, t.array(scoutingFormCodec)]);
 
 formsRouter.post("/", async (req, res) => {
   await pipe(
-    getFormsCollection(),
-    flatMap((collection) =>
-      pipe(
-        right(req),
-        createBodyVerificationPipe(combinedCodec),
-        fromEither,
-        map((combinedBody) => ({ collection, combinedBody })),
+    rightEither(req),
+    createBodyVerificationPipe(combinedCodec),
+    fromEither,
+    map((combinedBody) =>
+      Array.isArray(combinedBody) ? combinedBody : [combinedBody],
+    ),
+    filterOrElse(
+      (forms) => !isEmpty(forms),
+      () => ({
+        status: StatusCodes.BAD_REQUEST,
+        reason: `Inserted Empty Form Array`,
+      }),
+    ),
+    bindTo("forms"),
+    bind("collection", getFormsCollection),
+    tap(({ collection, forms }) =>
+      right(
+        collection.deleteMany({
+          $or: forms.map((form) => ({
+            scouterName: form.scouterName,
+            "match.number": form.match.number,
+            "match.type": form.match.type,
+            competition: form.competition,
+            teamNumber: form.teamNumber,
+          })),
+        }),
       ),
     ),
-    map(({ collection, combinedBody }) =>
-      Array.isArray(combinedBody)
-        ? collection.insertMany(combinedBody)
-        : collection.insertOne(combinedBody),
-    ),
+    map(({ collection, forms }) => collection.insertMany(forms)),
     fold(
       (error) => () =>
         Promise.resolve(res.status(error.status).send(error.reason)),
