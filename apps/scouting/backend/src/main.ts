a3bf5966f8523ad7d8331a134e41d5985a9a8020
YoniKiriaty <yonikiriaty@gmail.com>
2026-02-27 12:13:01 +0200

    Yoni - added https and certs

--- apps/scouting/backend/src/main.ts
+++ apps/scouting/backend/src/main.ts
@@ -1,6 +1,8 @@
 // בס"ד
 import express from "express";
 import { apiRouter } from "./routes";
+import fs from "fs";
+import https from "https";
 
 const app = express();
 
@@ -11,6 +13,14 @@ app.set("query parser", "extended");
 app.use(express.json());
 app.use("/api/v1", apiRouter);
 
+
+const options = {
+  key: fs.readFileSync("./cert-key.key"),
+  cert: fs.readFileSync("./certificate.crt"),
+};
+https.createServer(options, app).listen(443, () => {
+  console.log("HTTPS Server running on port 443");
+});
 app.listen(port, () => {
   console.log(`Production server running at http://localhost:${port}`);
 });
