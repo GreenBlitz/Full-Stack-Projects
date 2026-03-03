767e30a7d5be7d949c4343b72341fa75f8a05b7a
karnishein <214869392+karnishein@users.noreply.github.com>
2026-03-03 19:34:12 +0200

    karni- basic sorting algorithm ready

--- apps/scouting/frontend/src/strategy/tabs/Tinder.tsx
+++ apps/scouting/frontend/src/strategy/tabs/Tinder.tsx
@@ -11,39 +11,61 @@ export const Tinder: React.FC = () => {
   const [teamOrder, setTeamOrder] = useState<number[]>([]);
   const [indexOne, setIndexOne] = useState<number>(FIRST_INDEX);
   const [indexTwo, setIndexTwo] = useState<number>(SECOND_INDEX);
+  const [isSortComplete, setSortComlete] = useState<boolean>(true);
 
   useEffect(() => {
     fetchTeamNumbers().then(setTeamOrder).catch(console.error);
   }, []);
 
-  const handleChosen = (chosenIndex: number) => {
-    if (chosenIndex === indexOne) {
+  const handleChosen = (winnerIndex: number) => {
+    const nextTeamIndex = indexTwo + INCREMENT;
+    if (winnerIndex === indexOne) {
       const newOrder = [...teamOrder];
+
       [newOrder[indexOne], newOrder[indexTwo]] = [
         newOrder[indexTwo],
         newOrder[indexOne],
       ];
 
       setTeamOrder(newOrder);
-      setIndexOne(indexTwo);
-      setIndexTwo((prev) => prev + INCREMENT);
-    } else {
-      setIndexOne(indexTwo + INCREMENT);
     }
+    setIndexOne(indexTwo);
+    setIndexTwo(nextTeamIndex);
+
+    setSortComlete(nextTeamIndex >= teamOrder.length);
   };
+
   return (
     <>
       <div>
-        {[indexOne, indexTwo].map((teamIndex) => (
-          <button
-            key={teamOrder[teamIndex]}
-            onClick={() => {
-              handleChosen(teamIndex);
-            }}
-          >
-            choose me
-          </button>
-        ))}
+        <button
+          onClick={() => {
+            setSortComlete(false);
+          }}
+        >
+          start sorting!
+        </button>
+        {!isSortComplete && (
+          <div>
+            <p>
+              index one: {indexOne} index two: {indexTwo}
+            </p>
+            {[indexOne, indexTwo].map((teamIndex) => (
+              <button
+                key={teamOrder[teamIndex]}
+                onClick={() => {
+                  handleChosen(teamIndex);
+                }}
+              >
+                ::::choose me {teamOrder[teamIndex]} index: {teamIndex}
+              </button>
+            ))}
+
+            {teamOrder.map((teamNumber) => (
+              <p key={teamNumber}>{teamNumber}</p>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
