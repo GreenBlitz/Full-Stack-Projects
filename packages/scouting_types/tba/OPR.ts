2d1004ba93d72ea1af9619db6c365ad718e0e067
YoniKiriaty <yonikiriaty@gmail.com>
2026-03-12 20:50:27 +0200

    Yoni - mapped the entire entries

--- packages/scouting_types/tba/OPR.ts
+++ packages/scouting_types/tba/OPR.ts
@@ -1,9 +1,50 @@
 // בס"ד
 
+import { mapObject } from "@repo/array-functions";
 import * as t from "io-ts";
+
+export const COPR_TO_TBA_COPR = {
+  fuelTotal: "Hub Total Fuel Count",
+
+  fuelAuto: "Hub Auto Fuel Count",
+  fuelTele: "Hub Teleop Fuel Count",
+  fuelEndgame: "Hub Endgame Fuel Count",
+
+  fuelTransition: "Hub Transition Fuel Count",
+  fuelFirstActive: "Hub First Active Shift Count",
+  fuelSecondActive: "Hub Second Active Shift Count",
+
+  fuelShift1: "Hub Shift 1 Fuel Count",
+  fuelShift2: "Hub Shift 2 Fuel Count",
+  fuelShift3: "Hub Shift 3 Fuel Count",
+  fuelShift4: "Hub Shift 4 Fuel Count",
+
+  climbAuto: "autoTowerPoints",
+  climbEndgame: "endGameTowerPoints",
+
+  rp: "rp",
+  rpEnergized: "energizedAchieved",
+  rpSuperCharged: "superchargedAchieved",
+  rpTraversal: "traversalAchieved",
+
+  foulPoints: "foulPoints",
+  g206Penalty: "g206Penalty",
+  majorFoulCount: "majorFoulCount",
+  minorFoulCount: "minorFoulCount",
+
+  totalPoints: "totalPoints",
+  totalAutoPoints: "totalAutoPoints",
+  totalTeleopPoints: "totalTeleopPoints",
+  totalClimbPoints: "totalTowerPoints",
+
+  uncounted: "Hub Uncounted",
+  adjustPoints: "adjustPoints",
+} as const;
+
 export const teamOPRCodec = t.type({
   teamNumber: t.number,
   opr: t.number,
+  ...mapObject(COPR_TO_TBA_COPR, () => t.number),
 });
 
 export type TeamOPR = t.TypeOf<typeof teamOPRCodec>;
