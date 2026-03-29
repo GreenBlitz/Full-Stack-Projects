4ea74988f35c0c56b2913cc44dd8e22682838586
YoniKiriaty <yonikiriaty@gmail.com>
2026-03-29 18:41:57 +0300

    Yoni - builded to single file

--- apps/shift-tracker/frontend/vite.config.ts
+++ apps/shift-tracker/frontend/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    viteSingleFile(),
     tailwindcss(),
   ],
 });
