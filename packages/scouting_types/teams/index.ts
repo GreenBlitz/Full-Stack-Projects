94de59697e25ba96929e0343510c07374b202801
YoniKiriaty <yonikiriaty@gmail.com>
2026-04-03 08:41:40 +0300

    Yoni - fixed type imports

--- packages/scouting_types/teams/index.ts
+++ packages/scouting_types/teams/index.ts
@@ -9,8 +9,8 @@ import type {
   TeleClimb,
   TeleMovement,
 } from "../rebuilt";
-import { TeamOPR } from "../tba";
-import { EPA } from "../epa";
+import type { TeamOPR } from "../tba";
+import type { EPA } from "../epa";
 
 export const teamsProps = t.type({
   teams: t.union([t.array(t.number), t.number]),
