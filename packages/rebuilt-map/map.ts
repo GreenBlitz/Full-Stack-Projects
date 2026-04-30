86852a46716bb7c6c4c80351a8895a104cbc5edd
karnishein <214869392+karnishein@users.noreply.github.com>
2026-04-30 08:28:25 -0500

    Yoni - added flipped

--- packages/rebuilt-map/map.ts
+++ packages/rebuilt-map/map.ts
@@ -1,5 +1,5 @@
 // בס"ד
-import blue from "./images/blue-field-4418.png";
+import blue from "./images/blue-field-4418-flipped.png";
 export const blueField = blue;
-import red from "./images/red-field-4418.png";
+import red from "./images/red-field-4418-flipped.png";
 export const redField = red;
