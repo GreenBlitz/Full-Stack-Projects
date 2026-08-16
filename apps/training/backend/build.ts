4e41b804a92932d911c778e18fa92885e6598d73
karnishein <karni.shein@gmail.com>
2026-08-16 17:33:40 +0300

    added training (#168)

---
+++ apps/training/backend/build.ts
@@ -0,0 +1,33 @@
+// בס"ד
+import { build, context } from "esbuild";
+import { spawn } from "child_process";
+
+const isDev = process.env.NODE_ENV === "DEV";
+
+const bundlePath = "dist/bundle.js";
+
+const buildSettings = {
+  entryPoints: ["src/main.ts"],
+  outfile: "dist/bundle.js",
+  bundle: true,
+  plugins: [],
+  minify: true,
+  platform: "node",
+  target: ["ES2022"],
+  format: "cjs",
+  external: ["@repo/config-env"],
+} satisfies Parameters<typeof build>[0];
+
+const buildDev = async () =>
+  context(buildSettings)
+    .then(async (ctx) => ctx.watch())
+    .then(() => {
+      console.log("Starting node watch to manage execution of bundle.js");
+      spawn("node", ["--watch", bundlePath], { stdio: "inherit", shell: true });
+    });
+
+const buildedProject = isDev ? buildDev() : build(buildSettings);
+
+buildedProject.catch((error: unknown) => {
+  console.warn(error);
+});
