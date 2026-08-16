016fa64b9ca246e425f70f1c1608715b1dfd913d
Amit Kesar <dllekesar@gmail.com>
2026-08-16 20:26:30 +0300

    i did the first 5

---
+++ apps/training/backend/src/dayone.ts
@@ -0,0 +1,24 @@
+function greet(name: string):string {
+    return("hello"+ name);
+}
+
+function sum(a: number,b:number):number {
+const numcount= a+b
+    return(numcount);
+}
+
+function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
