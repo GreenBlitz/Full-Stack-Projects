fab55781662981bc442509841d0991b5c7f1f075
hillelv <hillel.vider@gmail.com>
2026-08-17 23:04:42 +0300

    hillel- all exercises

--- apps/training/backend/main.ts
+++ apps/training/backend/main.ts
@@ -27,13 +27,114 @@ function getFullName(firstName: string, lastName: string) {
 }
 
 function averageGrade(grades: number[]) {
-  let sum = 0;
-  grades.map((grade, i) => (sum += grade));
-  return sum / grades.length;
+  return sumArray(grades) / grades.length;
 }
 
 function isPalindrome(str: string) {
   return str === reverseString(str);
 }
 
-console.log(isPalindrome("dorod"));
+function findMaxValue(numbers: number[]): number {
+  let max = numbers[0];
+  numbers.forEach((number) => {
+    if (number > max) {
+      max = number;
+    }
+  });
+  return max;
+}
+
+function sumPositiveNumbers(numbers: number[]) {
+  let sum = 0;
+  numbers.forEach((number) => {
+    if (number > 0) {
+      sum += number;
+    }
+  });
+  return sum;
+}
+
+function getLongestString(words: string[]) {
+  let longestString = words[0];
+  words.forEach((word) => {
+    if (word.length > longestString.length) {
+      longestString = word;
+    }
+  });
+  return longestString;
+}
+
+function calculateDiscount(price: number, discountPercentage: number) {
+  return (price * (100 - discountPercentage)) / 100;
+}
+
+function findSecondLargest(numbers: number[]) {
+  const largestValue = findMaxValue(numbers);
+  const largestIndex = numbers.findIndex((number) => number === largestValue);
+  numbers[largestIndex] = -Number.MAX_SAFE_INTEGER;
+  const secondLargestValue = findMaxValue(numbers);
+  numbers[largestIndex] = largestValue;
+  return secondLargestValue;
+}
+
+function doubleArray(numbers: number[]) {
+  return numbers.map((number) => number * 2);
+}
+
+function positiveOnly(numbers: number[]) {
+  return numbers.filter((number) => number > 0);
+}
+
+function printAll(names: string[]) {
+  names.forEach((name) => console.log(name));
+}
+
+function hasEven(numbers: number[]) {
+  return numbers.some((number) => number % 2 === 0);
+}
+
+function firstLargerThanFifty(numbers: number[]) {
+  return numbers.find((number) => number > 50);
+}
+
+function isExist(numbers: number[], value: number) {
+  return numbers.includes(value);
+}
+
+function sumArray(numbers: number[]) {
+  return numbers.reduce((sum, number) => sum + number, 0);
+}
+
+function longerThanFive(strings: string[]) {
+  return strings.filter((string) => string.length > 5);
+}
+
+function reverseEach(strings: string[]) {
+  return strings.map(reverseString);
+}
+
+function allPositive(numbers: number[]) {
+  return numbers.every((number) => number > 0);
+}
+
+function sumAllEven(numbers: number[]) {
+  return sumArray(numbers.filter((number) => number % 2 === 0));
+}
+
+function squareAllPositive(numbers: number[]) {
+  return numbers
+    .filter((number) => number > 0)
+    .map((number) => number * number);
+}
+
+function largerThanTenAmount(numbers: number[]) {
+  return numbers.filter((number) => number > 10).length;
+}
+
+function doubledLargerThanHundred(numbers: number[]) {
+  return doubleArray(numbers.filter((number) => number > 100));
+}
+
+function analizeNumbers(numbers: number[]) {
+  return [sumArray(numbers), hasEven(numbers), positiveOnly(numbers)];
+}
