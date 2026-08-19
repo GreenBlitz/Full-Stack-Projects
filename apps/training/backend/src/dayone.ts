ac2994072f51c96ae343c32671f9312ebf9547f1
Amit Kesar <dllekesar@gmail.com>
2026-08-19 17:41:54 +0300

    danielle: 13Q

--- apps/training/backend/src/dayone.ts
+++ apps/training/backend/src/dayone.ts
@@ -3,22 +3,41 @@ function greet(name: string):string {
 }
 
 function sum(a: number,b:number):number {
-const numcount= a+b
+const numcount= a+b;
     return(numcount);
 }
 
-function 
+function factorial(n:number):number {
+const num2= n!;
+    return(num2);
+}
+ function isEven(n:number):boolean{
+    return n % 2 == 0;
+ }
 
+ function reversestring(sen: string){
+    let new= "";
+    for(let i = 1;i<sen.length+1;i++){
+        new += sen[sen.length-i];
+}
+return(new);
 
+console.log (new);
 
-
-
-
-
-
-
-
-
+function getFullName(firstname:string,lastname:string):string{
+const fullname= firstname + lastname;
+    return(fullname);
+}
+function averageGrade(grades:number[]):number{
+const average= grades/grades.length
+return(average)
+}
+function ispalindrom(str:string):boolean{
+    let new= "";
+    for(let i = 1;i<str.length+1;i++){
+        new += str[str.length-i];
+    return(new==str)   
+}
 
 
 }
