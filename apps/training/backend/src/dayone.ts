2e74cf62d79090af1830d12a4e80a251feceb9f3
unknown <idoarzi10@gmail,com>
2026-08-19 17:41:54 +0300

    Ido: did part 1 q 1 to 13 and part to q 1 to 6

--- apps/training/backend/src/dayone.ts
+++ apps/training/backend/src/dayone.ts
@@ -71,10 +71,8 @@ console.log(averageGrade(nums))
 8 
 function isPalindrome(str: string){
         let newbull = true
-    for (let i = 1; i < str.length + 1; i++) {
-        if (str[i] !== str[str.length-i]){
-            newbull = false
-        }
+     if (str !== reverseString(str)){
+        newbull = false
     }
     return newbull;
 }
@@ -82,3 +80,128 @@ function isPalindrome(str: string){
 console.log(isPalindrome("see"))
 
 9
+
+function findMaxEivar(numbers: number[]){
+    let max = numbers[0];
+    for (let i = 1; i < numbers.length ; i++){
+        if (numbers[i] > max){
+            max = numbers[i];
+        }
+    }
+    return max
+}
+
+const val: number[] = [4, 6, 7];
+console.log(findMaxEivar(val));
+
+10
+
+function sumPositiveNumbers(numbers: number[]) {
+    let pozsum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 0) {
+            pozsum += numbers[i]
+        }
+    }
+    return pozsum
+}
+
+const num2: number[] = [-7, 9, 10, -1, 70]
+console.log(sumPositiveNumbers(num2))
+
+11
+
+function getLongestString(words: string[]) {
+    let max_l = words[0].length;
+    for (let i = 1; i < words.length; i++) {
+        if (words[i].length > max_l) {
+            max_l = words[i].length;
+        }
+    }
+    return max_l;
+}
+
+const txt: string[] = ["hi", "what's up", "R U good?"];
+console.log(getLongestString(txt));
+
+12
+
+function calculateDiscount(price: number, discount: number) {
+    let r_dis = 1 - discount
+    r_dis = r_dis * price
+    return r_dis
+}
+
+const pri = 100
+const dis = 0.15
+console.log(calculateDiscount(pri, dis))
+
+13
+
+function findSecondLargest(numbers: number[]) {
+    let r_max = numbers[0];
+    let s_max = 0
+    for (let i = 1; i < numbers.length ; i++){
+        if (numbers[i] > r_max){
+            s_max = r_max
+            r_max = numbers[i];
+        }
+    }
+    return s_max
+}
+
+console.log(findSecondLargest(num2))
+
+//part 2
+
+console.log(" ")
+console.log("part 2")
+console.log(" ")
+
+1
+
+const val2: number[] = [1, 2, 4, 8, 16];
+const doubled = val2.map((num) => num * 2);
+console.log(doubled);
+
+
+2
+
+const val3: number[] = [1, -1, 2, -2, 3, -3];
+const filtered = val3.filter(num => num > 0);
+console.log(filtered);
+
+
+3
+
+const txt2s: string[] = ["I", "Im", "Hello", "what's up"];
+txt2s.forEach((txt2) => {
+    console.log(txt2);
+});
+
+console.log(" ")
+
+4
+
+const number: number[] = [1, 3, 5, 6];
+const someeven = number.some(num => num % 2 == 0);
+console.log(someeven)
+
+console.log(" ")
+
+5
+const number2: number[] = [10, 20, 30, 40, 50, 60, 70];
+const first_more_then_50 = number2.find(num => num > 50)
+console.log(first_more_then_50)
+
+console.log(" ")
+
+6
+
+function includes(numbers: number[], num) {
+    let re = false;
+    re = numbers.includes(num);
+    return re
+}
+
+console.log(includes(number2, 9))
