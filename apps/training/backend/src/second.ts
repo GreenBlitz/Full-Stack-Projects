b42967391125cb2b222c0b6395f6dbb33538eecc
Amit Kesar <dllekesar@gmail.com>
2026-08-23 16:46:01 +0300

    danielle q2

--- apps/training/backend/src/second.ts
+++ apps/training/backend/src/second.ts
@@ -1,4 +1,38 @@
-interface User {id: string; name:string;email:string}
-function getUserName(users):string{
-    return users.name
-}
+// interface User {id: string; name:string;email:string}
+// // function getUserName(user:User):string{
+// //     return user.name
+// // }
+
+// // const hi:User= {id: "7", name:"Ori",email:"dnkjdskjdkj@"}
+// // const hii:User= {id: "8", name:"Or",email:"dnkjdskjdkj@"}
+
+// // let users:User[]=[hi,hii];
+// // function verifyemails(allusers:User[]){
+// //     let y= true;
+// //     for(let i=0; )
+// //     for(let i=0; allusers.length;i++){
+// //         if (allusers.email!==){
+
+// //         }
+// //         else if(allusers[i].email!==hii.email){
+// //              y=false};
+// //         }
+// //     return(y)}
+// //     console.log (verifyemails(users))
+
+
+// function findUserByld(users:User[]){
+// let id=""
+//      for(let i=0;i<users.length;i++){
+//         if (users[id]= users[i]){
+//             return(users[id])}
+//         else if (users[id]= users[i]){
+//             return("undfined")}}
+// console.log(findUserByld(users))}
+    
+
+
+
+
+
+console.log("hello world")
