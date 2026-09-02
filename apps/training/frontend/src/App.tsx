dc99a19a6dcf3b3b7a77aef10ee4b8710b142fc3
zivmaayan <zivmaayan9811@gmail.com>
2026-09-02 18:16:57 +0300

    Ziv: Managed to work on the right version of Vite, working on the DuckCard css

--- apps/training/frontend/src/App.tsx
+++ apps/training/frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import { DuckCard } from "./components/DuckCard";
+
 // בס"ד
 function App() {
-  return (<p>hello</p>);
+  return (
+    <div>
+      <DuckCard name="Henry" color="white" age={15}></DuckCard>
+      <DuckCard name="Nahum" color="black" age={14}></DuckCard>
+      <DuckCard name="Zib" color="yellow" age={46}></DuckCard>
+    </div>
+  );
 }
 
 export default App;
--- apps/training/frontend/src/components/DuckCard.tsx
+++ apps/training/frontend/src/components/DuckCard.tsx
@@ -6,12 +6,14 @@ interface DuckCardProps {
 
 export function DuckCard({ name, color, age }: DuckCardProps) {
   return (
-    <>
+    <div className="aspect-2/3 m-px">
+      <link href="CSS.css" rel="stylesheet" />
       <span>name: {name}</span>
       <br />
       <span>color: {color}</span>
       <br />
       <span>age: {age}</span>
-    </>
+      <br />
+    </div>
   );
 }
