ff7beee4ff924175d0fb18f4f07f0e9c9506c9c2
zivmaayan <zivmaayan9811@gmail.com>
2026-09-06 20:29:13 +0300

    Ziv: I finished task 4 of react, in the middle of 5

--- apps/training/frontend/src/App.tsx
+++ apps/training/frontend/src/App.tsx
@@ -3,22 +3,22 @@ import { Duck, type DuckProps } from "./components/Duck";
 import { Ducks, type DucksProps } from "./components/Ducks";
 
 // בס"ד
-function deleteFinalDuck({ ducks: initialDucks }: DucksProps) {
-  const [deleteFinal, setDeleteFinal] = useState(initialDucks);
-  setDeleteFinal((prevDeleteFinal) => prevDeleteFinal.slice(0, -1));
-}
-
 function App() {
-  let ducks = [
+  const initialDucks = [
     { name: "Henry", color: "white", age: 15 },
     { name: "Nahum", color: "black", age: 14 },
     { name: "Zib", color: "yellow", age: 46 },
     { name: "Maor", color: "magenta", age: 16 },
   ];
+  const [ducks, setDucks] = useState(initialDucks);
+
+  
   return (
     <>
       <Ducks ducks={ducks} />
-      <button type="button" onClick={() => setDelete}
+      <button type="button" onClick={() => setDucks(ducks.slice(0, -1))}>
+        Delete Duck
+      </button>
     </>
   );
 }
