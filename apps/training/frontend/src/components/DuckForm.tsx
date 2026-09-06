eff7030f4c205d28cfdba9fdcd531b6e4ee81c29
zivmaayan <zivmaayan9811@gmail.com>
2026-09-06 20:30:16 +0300

    Ziv: I finished task 4 of react, in the middle of 5

--- apps/training/frontend/src/components/DuckForm.tsx
+++ apps/training/frontend/src/components/DuckForm.tsx
@@ -1,17 +1,21 @@
 import { Duck, type DuckProps } from "./Duck";
 
-function makeDuckProps({name, color, age}: DuckProps): DuckProps{
-    return {name, color, age}
+function makeDuckProps({ name, color, age }: DuckProps): DuckProps {
+  return { name, color, age };
 }
-export function DuckForm(){
-  return <form>
-    <label>Name: </label>
-    <input name="name" id="name" type="text" value="" required/><br/>
-    <label>Color: </label>
-    <input name="color" id="color" type="text" value="" required/><br/>
-    <label>Age: </label>
-    <input name="age" id="age" type="number" value={-1} required/><br/>
-    <br/><br/>
-    <button type="submit">Add Duck</button>
-  </form>;
+export function DuckForm() {
+  return (
+    <form>
+      <label>Name: </label>
+      <input name="name" id="name" type="text" value="" required />
+      <br />
+      <label>Color: </label>
+      <input name="color" id="color" type="text" value="" required />
+      <br />
+      <label>Age: </label>
+      <input name="age" id="age" type="number" value={-1} required />
+      <br />
+      <button type="submit">Add Duck</button>
+    </form>
+  );
 }
