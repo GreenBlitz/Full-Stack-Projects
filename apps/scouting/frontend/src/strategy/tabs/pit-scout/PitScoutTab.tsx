56c3e01573fbafe77fbd5b0fb16cba281c1595e3
GevaX <gevax1010@gmail.com>
2026-09-16 20:24:32 +0300

    Fix import

--- apps/scouting/frontend/src/strategy/tabs/pit-scout/PitScoutTab.tsx
+++ apps/scouting/frontend/src/strategy/tabs/pit-scout/PitScoutTab.tsx
@@ -7,7 +7,7 @@ import type {
   PitScoutBooleanMetric,
 } from "@repo/scouting_types";
 import { BooleanStats } from "./BooleanStats";
-import FRC_TEAMS from "@repo/frc/teams.json";
+import { FRC_TEAMS } from "@repo/frc";
 
 export const PIT_SCOUT_URL = "/api/v1/pit/";
 
