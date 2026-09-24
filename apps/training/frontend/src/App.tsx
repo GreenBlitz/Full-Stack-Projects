36edfabe6adbf9edaa943f316c690cf413e528ed
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2026-09-24 23:32:15 +0300

    added useState to ducks and a button that lets you deleate the last duck

--- apps/training/frontend/src/App.tsx
+++ apps/training/frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import { useState, type FC } from "react";
 import { DuckCard, type DuckCardProps } from "./components/DuckCard";
 import { Duck } from "./components/Ducks";
-const ducks: DuckCardProps[] = [
+const initialDucks: DuckCardProps[] = [
   { name: "Avi", color: "red", age: 4 },
   { name: "Moshe", color: "blue", age: 12 },
   { name: "Haim", color: "yellow", age: 9 },
@@ -10,7 +10,14 @@ const ducks: DuckCardProps[] = [
 ];
 
 const App: FC = () => {
-  return <Duck ducks={ducks} />;
+  const [ducks, setDucks] = useState(initialDucks)
+  return (
+    <>
+      <Duck ducks={ducks} />
+      <br />
+      <button className="remove-duck" type="button" onClick={() => setDucks(ducks.slice(0, -1))}>Remove a duck</button>
+    </>
+  );
 };
 
 export default App;
